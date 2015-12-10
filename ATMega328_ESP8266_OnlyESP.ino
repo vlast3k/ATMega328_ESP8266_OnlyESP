a94e49e4116f91a3c698762c380aea144e93160e
Vladi Vladi <vlast3k@gmail.com>
2015-12-10 22:10:33 +0200

    Allow to update only ESP directly

---
+++ ATMega328_ESP8266_OnlyESP.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+      pinMode(2, OUTPUT);
+      pinMode(9, INPUT);
+      pinMode(10, INPUT);
+     
+    digitalWrite(2, LOW);
+    delay(2000);
+    digitalWrite(2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
